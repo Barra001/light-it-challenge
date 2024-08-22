dee83edf7c6d3a1b1664850f247e0c0231559216
Manuel Barrabino <manubarrabino@gmail.com>
2024-08-22 11:12:46 -0300

    refactor

--- src/contexts/patientsContext.ts
+++ src/contexts/patientsContext.ts
@@ -2,5 +2,5 @@ import {createContext} from 'react';
 import Patient from '../models/patient';
 const patients:Patient[] = [];
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const setPatients = (patients: Patient[]) => {};
+const setPatients = (_patients: Patient[]) => {};
 export const PatientsContext = createContext({patients, setPatients});
